2a040cb2539df3a962bdb43b48f3e504e443baca
jdbuenol <jdbuenol@eafit.edu.co>
2023-05-03 18:01:05 -0500

    Docker compose and mount script added

--- installEFSTools.sh
+++ installEFSTools.sh
@@ -4,3 +4,4 @@ git clone https://github.com/aws/efs-utils
 cd ./efs-utils
 ./build-deb.sh
 apt-get -y install ./build/amazon-efs-utils*deb
+cd ..
---
+++ mount.sh
@@ -0,0 +1,5 @@
+nfs="fs-0b89621c6184cbb70"
+mkdir /moodle_data
+mount -t efs $nfs /moodle_data
+mkdir /moodle_data/moodle_data
+mkdir /moodle_data/moodle_datadata
